cb8ce9bf3429f6aa94834a02f876350759c2776d
Fernanda <fer_zavala2711@hotmail.com>
2019-12-04 17:56:20 -0600

    Implementación del perceptron

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,3 +8,10 @@ def run(num):
             print("running")
 greet("Hansel","Grettel")
 run(1,2,3,4,5)
+
+def perceptron(a, b, c, d):
+    if (a*b+c*d)>20:
+        print("1")
+    else:
+        print(0)
+preceptron(1, 2, 3, 4)

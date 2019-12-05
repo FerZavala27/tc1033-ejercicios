f3bc495a9872de9004ea676c9c33e69b71515608
Fernanda <fer_zavala2711@hotmail.com>
2019-12-04 19:15:42 -0600

    Implementando clase "Vehiculos"

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+class Vehiculo():
+    def _init_(self,_matricula,_medio,_pasajeros,_velocidad):
+        self._matricula=matricula
+        self._medio=medio
+        self._pasajeros=pasajeros
+        self._velocidad=velocidad
+        
+        

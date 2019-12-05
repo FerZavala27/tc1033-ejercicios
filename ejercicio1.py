95633a004939873aa272854046e9b16c96fd99ee
Fernanda <fer_zavala2711@hotmail.com>
2019-12-04 18:01:44 -0600

    Modificacion del main para usar perceptron
    También se corrigieron errores pasados

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,12 +6,22 @@ def run(num):
     if num>=0 and num<10:
         for x in (0, num):
             print("running")
-greet("Hansel","Grettel")
-run(1,2,3,4,5)
+greet("Hansel")
+greet("Grettel")
+run(1)
+run(2)
+run(3)
+run(4)
+run(5)
 
-def perceptron(a, b, c, d):
+def perceptron(a,b,c,d):
     if (a*b+c*d)>20:
         print("1")
     else:
         print(0)
-preceptron(1, 2, 3, 4)
+perceptron(1,5,1,5)
+perceptron(1,10,1,5)
+perceptron(1,5,1,10)
+
+
+

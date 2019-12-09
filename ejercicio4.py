f8cbd7b4f2874f98a49b4c6db30fb1d3d129eef8
FerZavala27 <57366877+FerZavala27@users.noreply.github.com>
2019-12-08 23:18:41 -0600

    Actualización ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,14 +1,27 @@
 class VehiclesUI():
     def registrar_vehiculos():
-        print("Ingrese la matricula del auto")
-        matricula=int(input())
-        print("Ingrese el modelo")
-        modelo=str(input())
-        print("Ingrese el nombre del fabricante")
-        fabricante=str(input())
+        lista=[]
+        mod=1
+        while mod==1:
+            mod==2
+            matricula=int(input("Ingrese la matricula a registrar"))
+            modelo=str(input("Ingrese el modelo"))
+            fabricante=str(input("ingrese el nombre del fabricante"))
+            opcion=int(input("Si desea continuar pulse 2, de otro modo para terminar pulse 1"))
+            if opcion==1:
+                resultado=VehiclesDP.__init__(matricula,modelo,fabricante)
+                break
+            if opcion==2:
+                VehiclesUI.registrar_vehiculos()
     
 
 class VehiclesDP():
+    def __init__(resp1,resp2,resp3):
+        print(resp1,resp2,resp3)
+
+
+VehiclesUI.registrar_vehiculos()    
+            
     
 
 

59ff36848e754651d3cae34e0f32f5d46fb18b61
FerZavala27 <57366877+FerZavala27@users.noreply.github.com>
2019-12-08 19:21:43 -0600

    EJERCICIO 4
    Implementación función registro de autos

---
+++ ejercicio4.py
@@ -0,0 +1,14 @@
+class VehiclesUI():
+    def registrar_vehiculos():
+        print("Ingrese la matricula del auto")
+        matricula=int(input())
+        print("Ingrese el modelo")
+        modelo=str(input())
+        print("Ingrese el nombre del fabricante")
+        fabricante=str(input())
+    
+
+class VehiclesDP():
+    
+
+
